577fb4c0c565fbe5377bae9f774d0c3da409efac
Hartley Brody <hartley.brody@gmail.com>
2014-03-06 22:34:54 -0500

    caesar cipher

---
+++ caesar.py
@@ -0,0 +1,34 @@
+import argparse
+
+def shift_message(message, shift):
+    encrypted = ""
+    for unshifted_letter in message:
+        encrypted += shift_letter(unshifted_letter, shift)
+    return encrypted
+
+def shift_letter(letter, shift):
+    if letter == " ": return " "
+    ordinal = ord(letter)
+    shifted_ordinal = ordinal + shift
+    if shifted_ordinal <= 97:
+        shifted_ordinal = shifted_ordinal + 26
+    if shifted_ordinal >= 123:
+        shifted_ordinal = shifted_ordinal - 26
+    return chr(shifted_ordinal)
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--message", required=True, help="What message are you trying to encrypt?")
+    parser.add_argument("-s", "--shift", type=int, required=True, help="What is the shift key for this message?")
+    parser.add_argument("-d", "--direction", required=True, help="Encrypt or Decrypt?")
+    args = parser.parse_args()
+    
+    if args.direction.lower() in ["e", "encrypt"]:
+        shift = args.shift
+    elif args.direction.lower() in ["d", "decrypt"]:
+        shift = 0-args.shift
+
+    print shift_message(args.message, shift)
